3f9199eb912d3a841c064963ace7f1004c9bcc4e
Artem Balan <a.s.balan96@gmail.com>
2024-01-28 20:31:19 +0200

    use @RestController instead of @ResponseBody

--- src/main/java/com/example/sqch10ex1/controllers/MainController.java
+++ src/main/java/com/example/sqch10ex1/controllers/MainController.java
@@ -1,20 +1,17 @@
 package com.example.sqch10ex1.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
     @GetMapping("/ciao")
-    @ResponseBody
     public String ciao() {
         return "Ciao!";
     }
