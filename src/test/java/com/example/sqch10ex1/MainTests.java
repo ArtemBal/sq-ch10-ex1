caaaca3b3faf7e7d9ad6c34430a3df09471b288a
Artem Balan <a.s.balan96@gmail.com>
2024-01-28 20:37:54 +0200

    write tests

--- src/test/java/com/example/sqch10ex1/MainTests.java
+++ src/test/java/com/example/sqch10ex1/MainTests.java
@@ -1,13 +1,33 @@
 package com.example.sqch10ex1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MainTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void helloTest() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello!"));
 	}
 
+	@Test
+	public void ciaoTest() throws Exception {
+		mockMvc.perform(get("/ciao"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Ciao!"));
+	}
 }
